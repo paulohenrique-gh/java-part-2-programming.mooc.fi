4b93e4dba54409bf8d2502acfedb3da699975340
Paulo Henrique <ph.1928@hotmail.com>
2023-06-04 17:16:26 -0300

    Added: Part 10: exercises 10.01 - 10.15

---
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -0,0 +1,13 @@
+
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        // you can create test code here. Call the methods that you'll implement
+        // during the course of this exercise
+
+        System.out.println("day of week: " + new Checker().isDayOfWeek("sun"));
+        System.out.println("vowels: " + new Checker().allVowels("oi"));
+        System.out.println("time: " + new Checker().timeOfDay("21:23:05"));
+    }
+}
